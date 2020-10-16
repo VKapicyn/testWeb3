b2deb3e98a03cddb34dbfda91b02a2ebf1d286c0
Vladislav <zveronym@gmail.com>
2020-10-16 11:43:38 +0300

    Update forMetamask.js

--- forMetamask.js
+++ forMetamask.js
@@ -1,26 +1,28 @@
 let contactAddress = `0xa137E35Dc62AD97c214a6078dD825A728cD78866`; 
 let web3js;
+let accounts;
 
-function connectMetamask() {
+async function connectMetamask() {
     const isMetaMaskInstalled = () => {
         const {ethereum} = window;
         return Boolean(ethereum&&ethereum.isMetaMask);
     };
     if(!isMetaMaskInstalled()){
         alert('метамаск отсутствует')
-    } else
+    } else {
+        //await window.ethereum.enable();
+        web3js = new Web3(web3.currentProvider)
+        accounts = await web3js.eth.getAccounts()
         alert('метамаск подключен')
+    }
 };
 
-function callAirdropFunc() {
-    web3js = new Web3(web3.currentProvider)
-
+async function callAirdropFunc() {
     transaction = ({
-        from: web3js.eth.accounts[0],
+        from: accounts[0],
         to: contactAddress,
         value: 10000000000000,
         gas: 250000
     });
-
-    web3js.eth.sendTransaction(transaction)
+    await web3js.eth.sendTransaction(transaction)
 }
