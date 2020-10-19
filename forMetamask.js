2e945ce1c7d6317305ccdd105eaee55c53fea4dc
Vladislav <zveronym@gmail.com>
2020-10-19 14:05:02 +0300

    Update forMetamask.js

--- forMetamask.js
+++ forMetamask.js
@@ -1,5 +1,4 @@
-//FIXME: замените на адрес размещенного контракта!
-let contactAddress = `0x1bd29B6dD80C20127203E632A9599Baa2fe1FA8c`; 
+let contactAddress = `0xa137E35Dc62AD97c214a6078dD825A728cD78866`; // FIXME: замените на адрес вашего контракта!
 let web3js;
 let accounts;
 
@@ -13,20 +12,17 @@ async function connectMetamask() {
     } else {
         await window.ethereum.enable();
         web3js = new Web3(web3.currentProvider)
-        accounts = web3js.givenProvider.selectedAddress
-        console.log(accounts)
         alert('метамаск подключен')
     }
 };
 
 async function callAirdropFunc() {
-    web3js = new Web3(web3.currentProvider);
     transaction = ({
         from: web3.currentProvider.selectedAddress,
         to: contactAddress,
-        value: 10000000000000,
-        gas: 2000000,
-        gasPrice: 50,
+        value: 100000000000000000,
+        gas: 100000,
+        gasPrice: 5,
     });
     await web3js.eth.sendTransaction(transaction)
 }
